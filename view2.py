9e11ca6fa53b68e1866952686d31342535ee0a9f
km4kfl <lkmcguire@proton.me>
2024-05-22 21:31:43 -0400

    Work on viewing data and fetching data.

--- view2.py
+++ view2.py
@@ -2,7 +2,11 @@ import numpy as np
 import pickle
 import time
 import matplotlib.pyplot as plt
+import matplotlib.colors as cmap
 import datetime as dt
+import scipy.ndimage as ndimage
+import scipy.signal as signal
+import os
 
 def enumerate_samples():
     with open('s3radio248.pickle', 'rb') as fd:
@@ -27,12 +31,13 @@ def enumerate_samples():
 def enumerate_channel():
     dtl = dt.datetime(2024, 5, 18, 14, 23, 0)
     dtl = dtl.timestamp()
+    z = 0
     with open('s3radio248.pickle', 'rb') as fd:
         fd.seek(2781259173)
         try:
             while True:
                 key, blob = pickle.load(fd)
-                print(key[1], dtl, fd.tell())
+                #print(key[1], dtl, fd.tell())
                 if key[1] < dtl:
                     continue                
                 for sample in blob:
@@ -45,9 +50,12 @@ def enumerate_channel():
                         points = np.zeros(len(channel), np.float64)
                         freqs = np.zeros(len(channel), np.float64)
                         for ndx, item in enumerate(channel):
-                            points[ndx] = item['b0']
+                            points[ndx] = item['b1']
                             freqs[ndx] = item['freq'] + freq
-                        yield points, freqs
+                        yield ts, points, freqs
+                z += 1
+                #if z > 20:
+                #    break
         except EOFError:
             pass
 
@@ -62,7 +70,7 @@ def build_mask():
     s = None
     sc = 0
     st = time.time()
-    for c, _ in enumerate_channel():
+    for _, c, _ in enumerate_channel():
         print(c)
         #plt.plot(c)
         if s is None:
@@ -94,18 +102,221 @@ def load_mask():
     
 #build_mask()
 
-px = []
-py = []
+def all_time_spectrum_plot():
+    px = []
+    py = []
 
-mask = load_mask()
-for c, f in enumerate_channel():
-    c *= mask
-    for x in range(len(c)):
-        px.append(f[x])
-        py.append(c[x])
+    mask = load_mask()
+    for ts, c, f in enumerate_channel():
+        c *= mask
+        for x in range(len(c)):
+            px.append(f[x])
+            py.append(c[x])
 
-plt.title('Spectrum Plot')
-plt.xlabel('frequency')
-plt.ylabel('magnitude')
-plt.scatter(px, py, s=0.1)
+    plt.title('Spectrum Plot')
+    plt.xlabel('frequency')
+    plt.ylabel('magnitude')
+    plt.scatter(px, py, s=0.1)
+    plt.show()
+
+def running_mean_plot():
+    mask = load_mask()
+    ts_least = None
+    ts_most = None
+    f_least = None
+    f_most = None
+
+    print('Scanning data for bounds...')
+    for ts, c, f in enumerate_channel():
+        if ts_least is None or ts < ts_least:
+            ts_least = ts
+        if ts_most is None or ts > ts_most:
+            ts_most = ts
+        for x in range(len(c)):
+            if f_least is None or f[x] < f_least:
+                f_least = f[x]
+            if f_most is None or f[x] > f_most:
+                f_most = f[x]
+        size_c = len(c)
+
+    f_delta = f_most - f_least
+    ts_delta = ts_most - ts_least
+
+    #
+    freq_bins = 4096
+    time_period = 60 * 60
+    time_period_high_index = int((ts_most - ts_least) / time_period)
+    time_period_count = time_period_high_index + 1
+    period_resolution = min(time_period_count, 4096)
+
+    m3d = np.zeros((time_period_count, freq_bins, 2), np.float64)
+    
+    print('Accumulating energy...')
+    for ts, c, cf in enumerate_channel():
+        ts_pos = round((ts - ts_least) / ts_delta * (period_resolution - 1))
+        for x in range(len(c)):
+            f = cf[x]
+            m = c[x]
+            f_pos = round((f - f_least) / f_delta * (freq_bins - 1))
+            m3d[ts_pos, f_pos, 0] += m
+            m3d[ts_pos, f_pos, 1] += 1.0
+    
+    m3d = m3d[:, :, 0] / m3d[:, :, 1]
+
+    plt.imshow(m3d)
+    plt.show()
+
+def sigma_plot():
+    mask = load_mask()
+    ts_least = None
+    ts_most = None
+    f_least = None
+    f_most = None
+
+    print('Scanning data...')
+    for ts, c, f in enumerate_channel():
+        if ts_least is None or ts < ts_least:
+            ts_least = ts
+        if ts_most is None or ts > ts_most:
+            ts_most = ts
+        for x in range(len(c)):
+            if f_least is None or f[x] < f_least:
+                f_least = f[x]
+            if f_most is None or f[x] > f_most:
+                f_most = f[x]
+        size_c = len(c)
+
+    f_delta = f_most - f_least
+    ts_delta = ts_most - ts_least
+
+    #
+    time_period = 60 * 5
+    time_period_high_index = int((ts_most - ts_least) / time_period)
+    time_period_count = time_period_high_index + 1
+    period_resolution = min(time_period_count, 4096)
+    freq_bins = period_resolution
+
+    # mean per channel
+    mpc = np.zeros((freq_bins, 2), np.float64)
+
+    print('Calculating mean energy per channel...')
+    for ts, c, cf in enumerate_channel():
+        #ts_pos = round((ts - ts_least) / ts_delta * (period_resolution - 1))
+
+        f_pos = np.round((cf - f_least) / f_delta * (freq_bins - 1))
+        f_pos = f_pos.astype(np.uint32)
+        mpc[f_pos, 0] += c
+        mpc[f_pos, 1] += 1.0
+
+        #for x in range(len(c)):
+        #    f = cf[x]
+        #    m = c[x]
+        #    f_pos = round((f - f_least) / f_delta * (freq_bins - 1))
+        #    mpc[f_pos, 0] += m
+        #    mpc[f_pos, 1] += 1.0
+
+    # sqrt((e - mean(e)) ** 2)
+
+    mpc = mpc[:, 0] / mpc[:, 1]
+
+    # sum per channel
+    spc = np.zeros((freq_bins, 2), np.float64)
+    
+    print('Calculating standard deviation per channel...')
+
+    for ts, c, cf in enumerate_channel():
+        #ts_pos = round((ts - ts_least) / ts_delta * (period_resolution - 1))
+
+        f_pos = np.round((cf - f_least) / f_delta * (freq_bins - 1))
+        f_pos = f_pos.astype(np.uint32)
+        spc[f_pos, 0] += (c - mpc[f_pos]) ** 2
+        spc[f_pos, 1] += 1.0
+
+        #for x in range(len(c)):
+        #    f = cf[x]
+        #    m = c[x]
+        #    
+        #    spc[f_pos, 0] += (m - mpc[f_pos]) ** 2
+        #    spc[f_pos, 1] += 1.0
+
+    # std deviation per channel
+    stdpc = np.sqrt(spc[:, 0] / spc[:, 1])
+
+    print('Calculating sigma per channel...')
+
+    sigma = np.zeros((freq_bins, period_resolution), np.float64)
+    m_energy = np.zeros((freq_bins, period_resolution), np.float64)
+
+    for ts, c, cf in enumerate_channel():
+        ts_pos = np.round((ts - ts_least) / ts_delta * (period_resolution - 1))
+        f_pos = np.round((cf - f_least) / f_delta * (freq_bins - 1))
+        ts_pos = ts_pos.astype(np.uint32)
+        f_pos = f_pos.astype(np.uint32)
+        a = np.abs((c - mpc[f_pos]) / stdpc[f_pos])
+        sigma[f_pos, ts_pos] = np.max(
+            [a, sigma[f_pos, ts_pos]],
+            axis=0
+        )
+
+        m_energy[f_pos, ts_pos] = np.max(
+            [c, m_energy[f_pos, ts_pos]],
+            axis=0
+        )
+
+        #for x in range(len(c)):
+        #    f = cf[x]
+        #    m = c[x]
+        #    f_pos = round((f - f_least) / f_delta * (freq_bins - 1))
+        #    sigma[f_pos, ts_pos] = max(
+        #        (m - mpc[f_pos]) / stdpc[f_pos],
+        #        sigma[f_pos, ts_pos]
+        #    )
+
+    with open('sigma.pickle', 'wb') as fd:
+        pickle.dump(sigma, fd)
+
+    with open('m_energy.pickle', 'wb') as fd:
+        pickle.dump(m_energy, fd)
+
+    #plt.ylabel('frequency')
+    #plt.xlabel('time')
+    #plt.imshow(sigma, extents=[1, 2, 3, 4])
+    #plt.show()
+
+    #plt.imshow(m3d)
+    #plt.show()
+
+sigma_plot()
+
+with open('sigma.pickle', 'rb') as fd:
+    sigma = pickle.load(fd)
+
+with open('m_energy.pickle', 'rb') as fd:
+    m_energy = pickle.load(fd)
+
+pattern = np.array([
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+])
+
+print(sigma.shape, pattern.shape)
+
+#sigma = signal.correlate2d(sigma, pattern, mode='full')
+
+#sigma[sigma < 6] = 0.0
+#sigma[sigma > 6] = 1.0
+
+#sigma = ndimage.gaussian_filter(sigma, 2.0)
+
+plt.imshow(m_energy)
+plt.show()
+
+plt.ylabel('frequency')
+plt.xlabel('time')
+plt.imshow(sigma, extent=[0, 6e9, 6e9, 70e6])
 plt.show()
--- view_fetch_new.py
+++ view_fetch_new.py
@@ -2,6 +2,8 @@ import pickle
 import boto3
 import pprint
 import io
+import datetime as dt
+import os
 
 def get_boto3_s3_client(cred_file: str, region='us-east-2'):
     with open(cred_file, 'r') as fd:
@@ -27,6 +29,8 @@ def main(cred_path: str, bucket_name: str, data_path: str):
 
     s3_online = []
 
+    total_bytes = 0
+
     while True:    
         print('Fetching S3 index.')
 
@@ -48,6 +52,7 @@ def main(cred_path: str, bucket_name: str, data_path: str):
             ct = float(parts[1])
             item = (key, ct)
             assert item not in s3_online
+            total_bytes += node['Size']
             s3_online.append((key, ct))
 
         if len(nodes) == 1000:
@@ -56,21 +61,34 @@ def main(cred_path: str, bucket_name: str, data_path: str):
         else:
             break
 
+    # I have this weird feeling Amazon is using 1000 instead of 1024.
+    print(f'Bucket has {total_bytes / 1000 / 1000 / 1000}-GB bytes.')
     print(f'Have index of {len(s3_online)} items.')
 
-    s3_fetched = set()
-
-    print('Scanning existing data to build index of downloaded bucket nodes..')
     try:
-        with open(data_path, 'rb') as fd:
-            try:
-                while True:
-                    s3_key, _ = pickle.load(fd)
-                    s3_fetched.add('-'.join([str(v) for v in s3_key]))
-            except EOFError:
-                pass
+        dmt = os.stat(data_path).st_mtime
+        dimt = os.stat(data_path + '.index').st_mtime
+
+        if dmt > dimt:
+            print('The index was out of date.')
+            raise FileNotFoundError()
+
+        with open(data_path + '.index', 'rb') as fd:
+            s3_fetched = pickle.load(fd)
     except FileNotFoundError:
-        pass
+        s3_fetched = set()
+
+        print('Scanning existing data to build index of downloaded bucket nodes..')
+        try:
+            with open(data_path, 'rb') as fd:
+                try:
+                    while True:
+                        s3_key, _ = pickle.load(fd)
+                        s3_fetched.add('-'.join([str(v) for v in s3_key]))
+                except EOFError:
+                    pass
+        except FileNotFoundError:
+            pass
     
     s3_online.sort(key=lambda item: item[1])
 
@@ -80,9 +98,13 @@ def main(cred_path: str, bucket_name: str, data_path: str):
                 print('skipped', key)
                 continue
 
+            s3_fetched.add(key)
+
             data_fd = io.BytesIO()
             
-            print('Downloading.', key)
+            ts = float(key.split('-')[1])
+
+            print('Downloading.', dt.datetime.fromtimestamp(ts))
             s3c.download_fileobj(bucket_name, key, data_fd)
             print('Appending.')
             data_fd.seek(0)
@@ -106,6 +128,9 @@ def main(cred_path: str, bucket_name: str, data_path: str):
             pickle.dump((
                 (key, data)
             ), plot_fd)
+
+    with open(data_path + '.index', 'wb') as fd:
+        pickle.dump(s3_fetched, fd)
 
 if __name__ == '__main__':
     main('z:\\nbfreqscan\\s3sak.txt', 'radio248', 's3radio248.pickle')
